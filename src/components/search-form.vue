ff537e4dbff5e9a922e4ac1ab712cdf13fdc2054
oLNidfwworld <kuvetov@bk.ru>
2025-02-04 15:47:24 +0300

    redo

--- src/components/search-form.vue
+++ src/components/search-form.vue
@@ -19,7 +19,7 @@ onMounted(function ( ) {
 })
 </script>
 <template> 
-  <form class="flex gap-3">
+  <form @submit.prevent.stop class="flex gap-3">
     <TextInput v-model="searchQuery" type="search" name="q" />
     <button type="submit">
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="24"
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/sputnik-monitor/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
